042362ab5fd0edae7d505dcdc9cd2539f470220e
Sathiya Sangar <34126794+sathiyasangar@users.noreply.github.com>
2020-02-17 12:54:28 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -96,8 +96,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sathiyasangar.p.s.0@gmail.com'
-EMAIL_HOST_PASSWORD = '1310199719122015'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 
 CRONJOBS = [
     ('* * * * *', 'Mysite.cron.my_cron_job', '>> test.log')
